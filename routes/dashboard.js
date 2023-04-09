a0d658e503b7b3cd87ee89281e45b615cd1fb9eb
mcoder01 <manu01141@gmail.com>
2023-04-09 23:01:46 +0200

    Add stored password list and improved look

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+let passwords = [
+  {
+    account: "google", 
+    password: "prova1234"
+  }, 
+  {
+    account: "facebook",
+    password: "ciao123456"
+  }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard');
+  var response = {"passwords": passwords, "success": 0};
+  res.render('dashboard', response);
+});
+
+router.post('/', function(req, res, next) {
+  var account = req.body.account;
+  var password = req.body.password;
+  console.log("Account: " + account + ", password: " + password);
+
+  //passwords.push({account: account, password: password});
+  var response = {passwords: passwords, success: 1};
+  res.render('dashboard', response);
 });
 
 module.exports = router;
