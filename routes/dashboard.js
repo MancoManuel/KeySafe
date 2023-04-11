999b06400552afcbe71b82a57eedb3914c13f7c4
mcoder01 <manu01141@gmail.com>
2023-04-11 13:46:48 +0200

    Add stored password control buttons

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -19,12 +19,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  var account = req.body.account;
-  var password = req.body.password;
-  console.log("Account: " + account + ", password: " + password);
+  var action = req.body.action;
+  var success = 0;
+  if (action == "add") {
+    var account = req.body.account;
+    var password = req.body.password;
+    passwords.push({account: account, password: password});
+    success = 1;
+  } else if (action == "edit") {
+    var index = req.body.index;
+    var password = req.body.password;
+    passwords[index].password = password;
+  } else if (action == "delete") {
+    var index = req.body.index;
+    passwords.splice(index, 1);
+  }
 
-  //passwords.push({account: account, password: password});
-  var response = {passwords: passwords, success: 1};
+  var response = {passwords: passwords, success: success};
   res.render('dashboard', response);
 });
 
